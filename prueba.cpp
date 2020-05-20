7a37c9469fc94ac6cc4606f01f705345ec7544ff
LAPTOP-OVD0K1SJ\josep <josepablitis@hotmail.com>
2020-05-19 20:28:31 -0500

    Primer commit

---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>  
+#include <windows.h>
+using namespace std;
+
+int main (){
+    setlocale(LC_ALL, "spanish"); 
+    cout << "Hello world!";
+}
